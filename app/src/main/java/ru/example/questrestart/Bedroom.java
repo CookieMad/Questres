257d2b56f7f66821b88ed2fcf7db7143c92b1a6b
CookieMad <litlelambyc@gmail.com>
2022-06-12 12:58:32 +0300

    ver2.1

--- app/src/main/java/ru/example/questrestart/Bedroom.java
+++ app/src/main/java/ru/example/questrestart/Bedroom.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Bedroom extends AppCompatActivity {
 
@@ -12,17 +13,21 @@ public class Bedroom extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bedroom);
+        if(Person.gotKey){
+            TextView text = findViewById(R.id.textView);
+            text.setText("Ты осматриваешься вокруг себя и видишь средневековое крестьянское убранство. Пол стены и потолок сделаны из темного дерева. Рядом с кроватью ты замечаешь сундук, а также дверь в противоположной стороне комнаты.");
 
+        }
     }
 
 
     public void hall(View view){
         Intent i = new Intent(Bedroom.this, Hall.class);
         startActivity(i);
-    }
+        }
 
     public void chest(View view) {
         Intent i = new Intent(Bedroom.this, Chest.class);
         startActivity(i);
-    }
+        }
 }
